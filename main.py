7d57a066b688efe10fd5265db9e9118da6c7e869
Jack Parrigon <jackparrigon@gmail.com>
2025-07-24 21:03:24 -0700

    add new function in main that allows ai to run specified functions

--- main.py
+++ main.py
@@ -8,10 +8,60 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_files_content import schema_get_file_content
-from functions.run_python import schema_run_python_files
-from functions.write_files import schema_write_files
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_files_content import schema_get_file_content, get_file_content
+from functions.run_python import schema_run_python_files, run_python_files
+from functions.write_files import schema_write_files, write_files
+
+
+def call_function(function_call_part, verbose=False):
+    function_mapping = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_files": run_python_files,
+        "write_files": write_files
+    }
+
+    for part in function_call_part:
+        function_name = part.name
+        function_args = part.args
+        actual_function = function_mapping[function_name]
+        function_result = actual_function("calculator", **function_args)
+
+
+        if function_name in function_mapping:
+            if verbose:
+                print(f"Calling function: {function_name}({function_args})")
+                return types.Content(
+                    role="tool",
+                    parts=[
+                        types.Part.from_function_response(
+                            name=function_name,
+                            response={"result": function_result},
+                        )
+                    ],
+                )
+            else:
+                print(f"Calling function: {function_name}")
+                return types.Content(
+                    role="tool",
+                    parts=[
+                        types.Part.from_function_response(
+                            name=function_name,
+                            response={"result": function_result},
+                        )
+                    ],
+                )
+        else:
+            return types.Content(
+                role="tool",
+                parts=[
+                    types.Part.from_function_response(
+                        name=function_name,
+                        response={"error": f"Unknown function: {function_name}"},
+                    )
+                ],
+            )
 
 def main():
 
@@ -65,17 +115,30 @@ def main():
     prompt_token = response.usage_metadata.candidates_token_count
     response_token = response.usage_metadata.candidates_token_count
     function_call = response.function_calls
+    function_call_result = call_function(function_call, verbose)
 
-    if verbose:
-        print(response.text)
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {prompt_token}")
-        print(f"Response tokens: {response_token}")
     if function_call:
-        for part in function_call:
-            print(f"Calling function: {part.name}({part.args})")
+        try:
+            response_data = function_call_result.parts[0].function_response.response
+
+            if verbose:
+                print(f"User prompt: {user_prompt}")
+                print(f"-> {response_data}")
+                print(f"Prompt tokens: {prompt_token}")
+                print(f"Response tokens: {response_token}")
+            else:
+                print(f"-> {response_data}")
+
+        except (AttributeError, IndexError):
+            print("Fatal Error: Function result missing critical attribute '.parts[0].function_response.response'")
     else:
-        print(response.text)
+        if verbose:
+            print(f"User prompt: {user_prompt}")
+            print(f"{response.txt}")
+            print(f"Prompt tokens: {prompt_token}")
+            print(f"Response tokens: {response_token}")
+        else:
+            print(response.txt)
 
 
 if __name__ == "__main__":
